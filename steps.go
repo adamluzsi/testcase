8a7fbd7e5fbdbbd0c1ea77a5eea58518f8c2205f
Adam Luzsi <adamluzsi@gmail.com>
2019-05-01 01:14:59 +0200

    make sure, that append not able to leak context outside of a certain T#Run block

--- steps.go
+++ steps.go
@@ -4,8 +4,12 @@ import "testing"
 
 type Steps []func(*testing.T)
 
+func (s Steps) Add(step func(*testing.T)) Steps {
+	return append(append(Steps{}, s...), step)
+}
+
 func (s Steps) Setup(t *testing.T) {
-	for _, step := range s {
-		step(t)
+	for _, steps := range s {
+		steps(t)
 	}
 }
--- steps_test.go
+++ steps_test.go
@@ -9,19 +9,18 @@ import (
 func TestSetup(t *testing.T) {
 	var value string
 
-	steps := testrunctx.Steps{}
-
+	var steps = testrunctx.Steps{}
 	t.Run(`on`, func(t *testing.T) {
-		steps := append(steps, func(t *testing.T) { value = "1" })
+		steps := steps.Add(func(t *testing.T) { value = "1" })
 
 		t.Run(`each`, func(t *testing.T) {
-			steps := append(steps, func(t *testing.T) { value = "2" })
+			steps := steps.Add(func(t *testing.T) { value = "2" })
 
 			t.Run(`nested`, func(t *testing.T) {
-				steps := append(steps, func(t *testing.T) { value = "3" })
+				steps := steps.Add(func(t *testing.T) { value = "3" })
 
 				t.Run(`layer`, func(t *testing.T) {
-					steps := append(steps, func(t *testing.T) { value = "4" })
+					steps := steps.Add(func(t *testing.T) { value = "4" })
 
 					t.Run(`it will setup and break down the right context`, func(t *testing.T) {
 						steps.Setup(t)
