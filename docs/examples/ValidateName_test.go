8d8777f2f8192f7e0121b1b23a2099ba4fd41694
Adam Luzsi <adamluzsi@gmail.com>
2020-03-11 23:57:00 +0100

    refact

--- docs/examples/ValidateName_test.go
+++ docs/examples/ValidateName_test.go
@@ -14,14 +14,12 @@ import (
 func TestValidateName(t *testing.T) {
 	s := testcase.NewSpec(t)
 
-	subject := func(t *testcase.T) error {
+	var subject = func(t *testcase.T) error {
 		return examples.ValidateName(t.I(`name`).(string))
 	}
 
 	s.When(`is perfect`, func(s *testcase.Spec) {
-		s.Let(`name`, func(t *testcase.T) interface{} {
-			return `The answer is 42`
-		})
+		s.LetValue(`name`, `The answer is 42`)
 
 		s.Then(`it will be accepted without a problem`, func(t *testcase.T) {
 			require.Nil(t, subject(t))
@@ -29,9 +27,7 @@ func TestValidateName(t *testing.T) {
 	})
 
 	s.When(`is really long`, func(s *testcase.Spec) {
-		s.Let(`name`, func(t *testcase.T) interface{} {
-			return strings.Repeat(`x`, 128+rand.Intn(42)+1)
-		})
+		s.LetValue(`name`, strings.Repeat(`x`, 128+rand.Intn(42)+1))
 
 		s.Then(`it will that the name is too long`, func(t *testcase.T) {
 			require.Equal(t, examples.ErrTooLong, subject(t))
