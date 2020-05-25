e0ff7a39663c9027b1b87c21915d998c53a0f0c6
Adam Luzsi <adamluzsi@gmail.com>
2020-05-25 21:53:16 +0200

    tools

--- tools.go
+++ tools.go
@@ -1,6 +1,8 @@
 // +build tools
 
-package toggler
+//go:generate rm -rf .tools
+//go:generate mkdir -p .tools
+package testcase
 
-//go:generate go build -o bin/mockgen github.com/golang/mock/mockgen
+//go:generate go build -o .tools/ github.com/golang/mock/mockgen
 import _ "github.com/golang/mock/gomock"
---
+++ tools.mod
@@ -0,0 +1,5 @@
+module github.com/adamluzsi/testcase
+
+go 1.14
+
+require github.com/golang/mock v1.4.3 // indirect
