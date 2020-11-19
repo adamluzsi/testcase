8cde82872de0c78d411ddb1629afcdb05ac37a00
Adam Luzsi <adamluzsi@gmail.com>
2020-11-19 22:52:35 +0100

    build fix

--- internal/generate.sh
+++ internal/generate.sh
@@ -1,7 +1,38 @@
 #!/usr/bin/env bash
 set -e
 
-mockgen -source generate.go -destination mocks.go -package internal
-sed -i '/^import /a \"testing\"' mocks.go
-sed -i '/^type MockTB struct/a testing.TB' mocks.go
-go fmt mocks.go 1>/dev/null
+(
+	type mockgen
+	type sed
+	type go
+) 1>/dev/null
+
+export OUT_PATH="mocks.go"
+
+function main() {
+	generateMock
+	updateMock
+	fmtFile
+}
+
+function generateMock() {
+	mockgen -source generate.go -destination "${OUT_PATH}" -package internal
+}
+
+function updateMock() {
+	local -a args=()
+
+	if [[ "$(uname -s)" =~ Linux* ]]; then
+		args+=("--in-place")
+	fi
+
+  # add testing.TB as dependency to MockTB
+	sed "${args[@]}" '/^import /a \"testing\"' "${OUT_PATH}"
+	sed "${args[@]}" '/^type MockTB struct/a testing.TB' "${OUT_PATH}"
+}
+
+function fmtFile() {
+	go fmt "${OUT_PATH}" 1>/dev/null
+}
+
+main "${@}"
