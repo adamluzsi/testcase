7e6d9a652e3a6351f501f28e21fd086b9b24d7f1
Adam Luzsi <adamluzsi@gmail.com>
2020-11-20 22:57:25 +0100

    docs

--- README.go
+++ README.go
@@ -4,41 +4,44 @@ Package testcase implements two approaches to help you to do nested BDD style te
 
 
 
+Introduction
+
+testcase framework Is meant to help the test writer focus on defining the behavior of a given testing subject,
+while helping remaining disciplined with the way the test is being made.
+This disciplined ways of working will later can be used to make changes and refactoring on the testing suite a breeze.
+The main goal of the project is to provide long term maintainability and re-usability in testing.
+
+
+
 Spec Variables
 
-in your spec, you can use the `*testcase.variables` object,
-for fetching values for your objects.
-Using them is gives you the ability to create value for them,
-only when you are in the right testing scope that responsible
-for providing an example for the expected value.
+In your spec, you can define and then later use variables that bound to test execution.
+These values lazy evaluated for each test case, and then cached till the life time of the test.
+These values are thread safe, concurrently running test don't have visibility to the other test's variables.
+As a benefit of this, you can use parallel execution whenever your test subject don't have side effects.
 
-In test case scopes you will receive a structure ptr called `*testcase.variables`
-which will represent values that you configured for your test case with `Let`.
-
-Values in `*testcase.variables` are safe to use during T#Parallel.
+On top of that, if you need to build a certain context that will ensure the variable content,
+you can reference to a variable earlier than you actually define it.
+This helps you to ensure that each edge case is covered properly,
+and you don't have leaking values from one test to an another.
+You can learn more about this under Spec#Let Spec#LetValue and Var.
 
 
 
 Spec Hooks
 
-Hooks help you setup common things for each test case.
-For example clean ahead, clean up, mock expectation configuration,
-and similar things can be done in hooks,
-so your test case blocks with `Then` only represent the expected result(s).
-
-In case you work with something that depends on side-effects,
-such as database tests, you can use the hooks,
-to create clean-ahead / clean-up blocks.
-
-Also if you use gomock, you can use the spec#Around function,
-to set up the mock with a controller, and in the teardown function,
-call the gomock.Controller#Finish function,
-so your test cases will be only about
-what is the different behavior from the rest of the test cases.
-
-It will panic if you use hooks or variable preparation in an ambiguous way,
-or when you try to access variable that doesn't exist in the context where you do so.
-It tries to panic with friendly and supportive messages, but that is highly subjective.
+When you composite a testing context,
+each testing context meant to have a description
+and a corresponding code that fulfils the described context.
+To do so, Spec#Let might not be enough.
+For this purpose you can use hooks that will be executed Before, After or Around the Spec#Test blocks.
+Each testing context inherit all the exiting hook from its parent context.
+Therefore setting up tests with common events should be easy as creating a Spec#Context with a hook.
+If additional events required you can do so in sub contexts.
+Hooks can be also used to do common cleanups before or after each test scenario.
+Defining a hook after a test assertion is not possible,
+as it would ruin consistency of the testing scope.
+You will receive a warning about this if you attempt to do so.
 
 */
 package testcase
