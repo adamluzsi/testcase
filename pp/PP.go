03111482e9ee22f7361ad1143c3c050f6ba929ca
Adam Luzsi <adamluzsi@gmail.com>
2022-08-12 20:05:44 +0200

    add ability to label values during pretty printing

--- pp/PP.go
+++ pp/PP.go
@@ -8,21 +8,37 @@ import (
 
 var defaultWriter io.Writer = os.Stderr
 
-func PP(vs ...any) {
-	FPP(defaultWriter, vs...)
+type labelled struct {
+	Label string
+	Value any
 }
 
-func FPP(w io.Writer, vs ...any) {
+// L allows to label a value for pretty printing with PP.
+func L(label string, v any) labelled {
+	return labelled{
+		Label: label,
+		Value: v,
+	}
+}
+
+func PP(vs ...any) {
+	_, _ = FPP(defaultWriter, vs...)
+}
+
+func FPP(w io.Writer, vs ...any) (int, error) {
 	var (
 		form string
 		args []any
 	)
-	for i, v := range vs {
-		if i != 0 {
-			form += "\t"
+	for _, v := range vs {
+		switch v := v.(type) {
+		case labelled:
+			form += "%s\t%s\n"
+			args = append(args, v.Label, Format(v.Value))
+		default:
+			form += "%s\n"
+			args = append(args, Format(v))
 		}
-		form += "%s"
-		args = append(args, Format(v))
 	}
-	fmt.Fprintf(w, form+"\n", args...)
+	return fmt.Fprintf(w, form, args...)
 }
--- pp/PP_test.go
+++ pp/PP_test.go
@@ -10,11 +10,33 @@ func TestFPP(t *testing.T) {
 	buf := &bytes.Buffer{}
 	v1 := time.Date(2022, time.July, 26, 17, 36, 19, 882377000, time.UTC)
 	v2 := "foo"
-	FPP(buf, v1, v2)
+	n, err := FPP(buf, v1, v2)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
 
-	exp := "time.Date(2022, time.July, 26, 17, 36, 19, 882377000, time.UTC)\t\"foo\"\n"
+	exp := "time.Date(2022, time.July, 26, 17, 36, 19, 882377000, time.UTC)\n\"foo\"\n"
+	if len([]byte(exp)) != n {
+		t.Fatal("not everything was written out")
+	}
+
 	act := buf.String()
 	if act != exp {
+		t.Logf("got: %#v", act)
+		t.Fatalf("exp:\n%s\n\nact:\n%s", exp, act)
+	}
+}
+
+func TestFPP_withLabel(t *testing.T) {
+	buf := &bytes.Buffer{}
+	v1 := time.Date(2022, time.July, 26, 17, 36, 19, 882377000, time.UTC)
+	v2 := "foo"
+	FPP(buf, L("v1", v1), L("v2", v2))
+
+	exp := "v1\ttime.Date(2022, time.July, 26, 17, 36, 19, 882377000, time.UTC)\nv2\t\"foo\"\n"
+	act := buf.String()
+	if act != exp {
+		t.Logf("got: %#v", act)
 		t.Fatalf("exp:\n%s\n\nact:\n%s", exp, act)
 	}
 }
@@ -30,9 +52,10 @@ func TestPP(t *testing.T) {
 	v2 := "bar"
 	PP(v1, v2)
 
-	exp := "time.Date(2022, time.July, 26, 17, 36, 19, 882377000, time.UTC)\t\"bar\"\n"
+	exp := "time.Date(2022, time.July, 26, 17, 36, 19, 882377000, time.UTC)\n\"bar\"\n"
 	act := buf.String()
 	if act != exp {
+		t.Logf("got: %#v", act)
 		t.Fatalf("exp:\n%s\n\nact:\n%s", exp, act)
 	}
 }
