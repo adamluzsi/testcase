026e725f538aa47b3c6f91e1c108f18b98ea1e99
Adam Luzsi <adamluzsi@gmail.com>
2022-09-18 01:49:45 +0200

    add support for round tripper var creation

--- httpspec/RoundTripperMiddleware.go
+++ httpspec/RoundTripperMiddleware.go
@@ -1,13 +1,10 @@
 package httpspec
 
 import (
-	"bytes"
 	"context"
+	"github.com/adamluzsi/testcase"
 	"io"
 	"net/http"
-	"testing"
-
-	"github.com/adamluzsi/testcase"
 )
 
 func ItBehavesLikeRoundTripperMiddleware(s *testcase.Spec, subject MakeRoundTripperFunc) {
@@ -82,49 +79,4 @@ func (c RoundTripperMiddlewareContract) Spec(s *testcase.Spec) {
 			t.Must.Equal(expectedBody.Get(t), string(actualBody))
 		})
 	})
-}
-
-type RoundTripperFunc func(r *http.Request) (*http.Response, error)
-
-func (fn RoundTripperFunc) RoundTrip(request *http.Request) (*http.Response, error) {
-	return fn(request)
-}
-
-type RoundTripperDouble struct {
-	// RoundTripperFunc is an optional argument in case you want to stub the response
-	RoundTripperFunc RoundTripperFunc
-	// ReceivedRequests hold all the received http request.
-	ReceivedRequests []*http.Request
-}
-
-func (d *RoundTripperDouble) RoundTrip(r *http.Request) (*http.Response, error) {
-	d.ReceivedRequests = append(d.ReceivedRequests, r.Clone(r.Context()))
-	if d.RoundTripperFunc != nil {
-		return d.RoundTripperFunc(r)
-	}
-
-	const code = http.StatusOK
-	return &http.Response{
-		Status:           http.StatusText(code),
-		StatusCode:       code,
-		Proto:            "HTTP/1.0",
-		ProtoMajor:       1,
-		ProtoMinor:       0,
-		Header:           http.Header{},
-		Body:             io.NopCloser(bytes.NewReader([]byte{})),
-		ContentLength:    0,
-		TransferEncoding: nil,
-		Close:            false,
-		Uncompressed:     false,
-		Trailer:          nil,
-		Request:          r,
-		TLS:              nil,
-	}, nil
-}
-
-func (d *RoundTripperDouble) LastReceivedRequest(tb testing.TB) *http.Request {
-	if len(d.ReceivedRequests) == 0 {
-		tb.Fatalf("%T did not received any *http.Request", *d)
-	}
-	return d.ReceivedRequests[len(d.ReceivedRequests)-1]
 }
